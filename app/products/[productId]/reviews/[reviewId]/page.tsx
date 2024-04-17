85d95232d2d7ecf22f224cc10f12e346f563c74b
Rachit <rachit.gupta4@gmail.com>
2024-04-17 17:03:15 +0200

    route grouping and not-found file

--- app/products/[productId]/reviews/[reviewId]/page.tsx
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,10 @@
+import { notFound} from "next/navigation";
+
 export default function ProductReviewPage({params}: {params: {productId: string, reviewId: string}}) {
+
+  if(parseInt(params.reviewId) > 10){
+  notFound();
+  }
   return (
     <div>
       Review {params.reviewId} for product {params.productId}
